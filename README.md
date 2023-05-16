5fdb699eb799a1a48a08ca1281b6404b36c51359
Nikita Akimov <korchiy@yandex.ru>
2023-05-16 17:02:28 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# inkscape_batch_guides
-Inkscape batch guides processing
+# Inkscape: Batch Guides
+
+Extension for vector graphical editor Inkscape.
+
+Extension functionality
+-
+Move all guides (guidelines) on custom offset.
+
+Guides moves separately. Horizontal guides moved vertically and vertical guides moved horizontally.
+
+Extension version
+-
+1.0.0.
+
+For Inkscape version
+-
+1.2
+
+Location
+-
+Main menu - Extensions - Guides - Batch Guidelines
+
+Installation
+-
+- Copy all the extension files to the "extension" Inkscape directory.
+
+
+    c:\Users\_USER_NAME_\AppData\Roaming\inkscape\extensions\
+
+- Restart Inkscape
---
+++ batch_guidelines.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Nikita Akimov
+# interplanety@interplanety.org
+#
+# GitHub
+#    https://github.com/Korchy/inkscape_batch_guides
+
+import sys
+from inkex import EffectExtension
+
+# for debug output
+def eprint(*args, **kwargs):
+    # print to stderr
+    print(*args, file=sys.stderr, **kwargs)
+
+class Guidelines(EffectExtension):
+
+    def effect(self):
+        # main method for Effect action
+        for guide in self.guides(direction=self.options.guide_direction):
+            # inkscape v 1.2
+            guide.move_to(
+                pos_x=guide.point.x + (self.options.offset if guide.is_vertical else 0.0),
+                pos_y=guide.point.y - (self.options.offset if guide.is_horizontal else 0.0)
+            )
+            # inkscape v 1.3
+            # guide.set_position(
+            #     pos_x=guide.raw_position.x + (self.options.offset if guide.is_vertical else 0.0),
+            #     pos_y=guide.raw_position.y + (self.options.offset if guide.is_horizontal else 0.0)
+            # )
+
+    def guides(self, direction: str = None):
+        # get guides by direction ['vertical', 'horizontal', None]
+        if direction == 'vertical':
+            return (guide for guide in self.svg.namedview.get_guides() if guide.is_vertical)
+        elif direction == 'horizontal':
+            return (guide for guide in self.svg.namedview.get_guides() if guide.is_horizontal)
+        else:
+            return self.svg.namedview.get_guides()
+
+    def add_arguments(self, pars):
+        # parse arguments from the UI
+        pars.add_argument(
+            '--offset',
+            type=float,
+            default=0.0,
+            help='Offset'
+        )
+        pars.add_argument (
+            '--guide_direction',
+            type=str,
+            default='vertical',
+            help='Guide Direction'
+        )
+
+if __name__ == '__main__':
+    Guidelines().run()
