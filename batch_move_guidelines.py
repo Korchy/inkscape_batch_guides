3281e3063f7a0e36d2fe2de37e208315847f217f
Nikita Akimov <korchiy@yandex.ru>
2023-10-12 08:22:49 +0300

    update

--- batch_move_guidelines.py
+++ batch_move_guidelines.py
@@ -8,6 +8,7 @@
 #    https://github.com/Korchy/inkscape_batch_guides
 
 import sys
+import inkex
 from inkex import EffectExtension
 
 
@@ -22,16 +23,18 @@ class Guidelines(EffectExtension):
     def effect(self):
         # main method for Effect action
         for guide in self.guides(direction=self.options.guide_direction):
-            # inkscape v 1.2
-            # guide.move_to(
-            #     pos_x=guide.point.x + (self.options.offset if guide.is_vertical else 0.0),
-            #     pos_y=guide.point.y - (self.options.offset if guide.is_horizontal else 0.0)
-            # )
-            # inkscape v 1.3
-            guide.set_position(
-                pos_x=guide.position.x + (self.options.offset if guide.is_vertical else 0.0),
-                pos_y=guide.position.y + (self.options.offset if guide.is_horizontal else 0.0)
-            )
+            if inkex.__version__[:3] == '1.2':
+                # inkscape v 1.2
+                guide.move_to(
+                    pos_x=guide.point.x + (self.options.offset if guide.is_vertical else 0.0),
+                    pos_y=guide.point.y - (self.options.offset if guide.is_horizontal else 0.0)
+                )
+            else:
+                # inkscape v 1.3
+                guide.set_position(
+                    pos_x=guide.position.x + (self.options.offset if guide.is_vertical else 0.0),
+                    pos_y=guide.position.y + (self.options.offset if guide.is_horizontal else 0.0)
+                )
 
     def guides(self, direction: str = None):
         # get guides by direction ['vertical', 'horizontal', None]
